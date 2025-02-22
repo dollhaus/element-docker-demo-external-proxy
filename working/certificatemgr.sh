5b916c0900f3f883e2cadcfc035270b805dfccce
Andreas Doll <33754407+dollhaus@users.noreply.github.com>
2025-02-22 13:12:14 +0000

    add todo

--- working/certificatemgr.sh
+++ working/certificatemgr.sh
@@ -1,1 +1,6 @@
+# TODO
 
+# convert fullchain.pem and other certificate files in this directory to ca-certificates.crt and other appropriate formats
+# copy converted certs to <vol>/data/shares/certificateshares
+# install script as cron job or systemd unit and repeat daily
+# figure out how to hook into files that have recently changed and create a listener to only do the above when new certs are dropped
